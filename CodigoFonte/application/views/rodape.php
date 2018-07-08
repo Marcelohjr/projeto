3ddcde2901af14fe9741e6916c455f4cc7b6a783
Marcelohjr <38004533+Marcelohjr@users.noreply.github.com>
2018-07-08 11:53:19 -0300

    Add files via upload

--- CodigoFonte/application/views/rodape.php
+++ CodigoFonte/application/views/rodape.php
@@ -1,7 +1,19 @@
-    <br><br>
-<!-- Footer -->
-    <footer class="ft">
-      <div class="ct">
-        <p class="m-0 text-center text-white">Copyright &copy; SublimeStore 2018</p>
-      </div>
-    </footer>
+
+<br><br>
+<link href="css/login/styleCustom.css" rel="stylesheet">
+<!-- Footer -->	
+
+	
+	<footer class="footer" style="position:absolute; background-color: #000000; width:100%; margin-top: 5%;">
+		<div class="content has-text-centered" style="display: block; margin-bottom: 5%;" align="center">
+			<figure class="image is-50x50">
+				<a href="http://www.facebook.com" target="_blank"><img src="imagens/facebook.png" alt="facebook" height="40px" width="40px" ></a> &nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="http://www.gmail.com" target="_blank"><img src="imagens/google.png" alt="gmail" height="40px" width="40px" ></a>&nbsp;&nbsp;&nbsp;&nbsp;
+				<a href="http://www.twitter.com" target="_blank"><img src="imagens/twitter.png" alt="Email" height="40px" width="40px"></a>
+			</figure>
+		</div>
+			<p style="color: #871F78; font-size:14; text-align: center; font-size: arial;">Contato:</p>
+			<p style="color: #871F78; font-size:14; text-align: center;">(38)3621-2098</p><br><br>
+			<p class="m-6 text-center text-white" style="color: white; font-size:12; text-align: center;">Copyright &copy; SublimeStore 2018 LTDA</p>
+	</footer>
+	
